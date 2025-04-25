200849f4a82610bb27b0bab5377a9fdf8dc285dd
NoFate35 <vsokol35@mail.ru>
2025-04-25 17:38:55 +0300

    Changes to be committed: 	modified:   hexlet_testing/monkeypatching.py

--- hexlet_testing/monkeypatching.py
+++ hexlet_testing/monkeypatching.py
@@ -1,7 +1,36 @@
-from github import Github
+from unittest.mock import Mock
 
 
-def get_private_fork_names(username):
-    client = Github("access_token")
-    repos = client.get_user(username).get_repos(type="private")
-    return [repo.name for repo in repos if repo.fork]
+class MyObject:
+    def __init__(self, dependency):
+        self.dependency = dependency
+
+    def my_method(self, arg):
+        # какой-то метод, который зависит от зависимости
+        return self.dependency.some_method(arg)
+
+class Dependency():
+    def __init__(self, arg):
+        self.arg = arg
+    def rrr():
+        return self.arg
+        
+# Создаем мок-объект для зависимости
+mock_dependency = Mock()
+
+# Создаем объект, который мы будем тестировать
+my_object = MyObject(mock_dependency)
+
+# Имитируем поведение зависимости в мок-объекте
+mock_dependency.some_method.return_value = 42
+
+# Тестируем метод объекта
+result = my_object.my_method("some argument")
+print("resultttt", result)
+
+# Проверяем, что метод объекта вызвал метод зависимости с правильным аргументом
+print("yyyyyy", mock_dependency.some_method.assert_called_once_with("some argument"))
+
+
+# Проверяем, что метод объекта вернул правильный результат
+assert result == 42
